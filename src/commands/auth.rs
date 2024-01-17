6822f4bf32d823ac0eebe2df02ab6b3a6546c6dd
schlanqe <ilhanburcakbas@gmail.com>
2024-01-17 15:02:00 +0300

    host arg

--- src/commands/auth.rs
+++ src/commands/auth.rs
@@ -1,11 +1,14 @@
 use clap::Args;
+use crate::config::config::DEFAULT_API_URL;
 
 #[derive(Args, Debug)]
 pub struct AuthArgs {
-    #[arg(short, long)]
+    #[arg(short, long, required = true)]
     pub key: Option<String>,
+    #[arg(long, hide = true, default_value = DEFAULT_API_URL)]
+    pub host: Option<String>,
 }
 
 pub fn auth(args: &AuthArgs) {
-    println!("Authentication Key: {:?}", args.key);
+    println!("Authentication Key: {:?}\nApi Url: {:?}", args.key, args.host);
 }
