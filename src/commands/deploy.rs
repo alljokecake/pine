065de7c2053595fdeebdd946424c5722e3a2e040
schlanqe <ilhanburcakbas@gmail.com>
2024-01-18 15:02:12 +0300

    flags: deploy

--- src/commands/deploy.rs
+++ src/commands/deploy.rs
@@ -3,8 +3,21 @@ use std::path::PathBuf;
 
 #[derive(Args, Debug)]
 pub struct DeployArgs {
+    /// Relative path to a squid manifest file
     #[arg(short, long, default_value = "squid.yaml")]
-    pub manifest: PathBuf,
+    pub manifest: Option<PathBuf>,
+    /// Organization
+    #[arg(short, long)]
+    pub org: Option<String>,
+    /// Do a hard reset before deploying
+    #[arg(short, long = "hard-reset", num_args = 0)]
+    pub reset: (),
+    /// Don't require a confirmation if the version already exists
+    #[arg(short, long, num_args = 0)]
+    pub update: (),
+    /// Don't attach and stream squid logs after the deploy
+    #[arg(long = "no-stream-logs", num_args = 0)]
+    pub no_stream_logs: (),
 }
 
 pub fn deploy(args: &DeployArgs) {
