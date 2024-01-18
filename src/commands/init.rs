089a8149b8ce91494b04ef516f6bbe9a5ef039d1
schlanqe <ilhanburcakbas@gmail.com>
2024-01-18 17:45:32 +0300

    args: init

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,9 +1,20 @@
 use clap::Args;
+use std::path::PathBuf;
 
 #[derive(Args, Debug)]
 pub struct InitArgs {
+    /// The squid name
+    #[arg()]
+    pub name: Option<String>,
+    /// Target location for the squid.
     #[arg(short, long)]
-    pub default: Option<String>,
+    pub dir: Option<PathBuf>,
+    /// Clean up the target directory if exists
+    #[arg(short, long, num_args = 0)]
+    pub remove: (),
+    /// A template for the squid
+    #[arg(short, long)]
+    pub template: Option<String>,
 }
 
 pub fn init(args: &InitArgs) {
