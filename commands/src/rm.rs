63a6583542f936f573eff9c34bf361c20dbe62a3
schlanqe <ilhanburcakbas@gmail.com>
2024-01-20 15:18:56 +0300

    flags: rm

--- commands/src/rm.rs
+++ commands/src/rm.rs
@@ -2,8 +2,13 @@ use clap::Args;
 
 #[derive(Args, Debug)]
 pub struct RmArgs {
-    #[arg(short, long)]
-    pub default: Option<String>,
+    /// <name> or <name@version>
+    #[arg()]
+    pub name_and_version: Option<String>,
+
+    /// Does not prompt before removing a squid or its version
+    #[arg(short, long, num_args = 0)]
+    pub force: (),
 }
 
 pub fn rm(_args: &RmArgs) {
